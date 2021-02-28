f36afa20111cedce9ae5e26f2d0329245ad52dd0
Hayley Manke <hmanke1010@gmail.com>
2021-02-28 14:06:13 -0500

    Initial commit

---
+++ R/hw04phayleymanke10-package.R
@@ -0,0 +1,16 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
+## usethis namespace: start
+#' @importFrom tibble tibble
+## usethis namespace: end
+NULL
+## usethis namespace: start
+#' @importFrom ggplot2 ggplot aes geom_line
+## usethis namespace: end
+NULL
--- R/myseq_n_df.R
+++ R/myseq_n_df.R
@@ -1,4 +1,4 @@
-#' Recursive Sequence
+#' Recursive Sequence Plot
 #'
 #' @param x Numeric vector
 #' @param n Integer
@@ -7,7 +7,7 @@
 #' @export
 #'
 #' @examples
-myseq_n <- function(x, n) {
+myseq_n_df <- function(x, n) {
   if (length(x) == 3 && is.numeric(x) && n > 0) {
     nvec <- vector(mode = "double", length = n)
     for (i in seq_along(nvec)) {
@@ -20,7 +20,11 @@ myseq_n <- function(x, n) {
       }
     }
   }
-  return(nvec[n])
+  nvector <- nvec[3:n]
+  n <- 3:n
+  df <- data.frame(nvector, n)
+  df %>%
+    ggplot2::ggplot(mapping = ggplot2::aes(x = n, y = nvector)) +
+    ggplot2::geom_line()
 
 }
-
